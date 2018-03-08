37c399aa322fecaea66b9e988bad99824bb0b396
YutaSugao <mrfeather@YutaSugaonoMacBook.local>
2018-03-08 17:26:00 +0900

    Start swift

---
+++ Tinder/ViewController.swift
@@ -0,0 +1,111 @@
+//
+//  ViewController.swift
+//  Tinder
+//
+//  Created by YutaSugao on 2018/03/06.
+//  Copyright © 2018年 YutaSugao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var basicCard: UIView!
+    @IBOutlet weak var likeImageView: UIImageView!
+    
+    @IBOutlet weak var person1: UIView!
+    @IBOutlet weak var person2: UIView!
+    @IBOutlet weak var person3: UIView!
+    @IBOutlet weak var person4: UIView!
+    
+    let name = ["ほのか","あかね","みほ","カルロス"]
+    var likedName = [String]()
+    
+    var centerOfCard:CGPoint!
+    var people = [UIView]()
+    var selectedCardCount: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        centerOfCard = basicCard.center
+        people.append(person1)
+        people.append(person2)
+        people.append(person3)
+        people.append(person4)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func resetCard() {
+        basicCard.center = centerOfCard
+        basicCard.transform = .identity
+    }
+
+    
+    @IBAction func swipeCard(_ sender: UIPanGestureRecognizer) {
+        let card = sender.view!
+        let point = sender.translation(in:  view)
+        
+        card.center = CGPoint(x: card.center.x + point.x, y: card.center.y + point.y)
+        people[selectedCardCount].center = CGPoint(x: card.center.x + point.x, y: card.center.y + point.y)
+        
+        //角度を変える
+        let xFromCenter = card.center.x - view.center.x
+        card.transform = CGAffineTransform(rotationAngle: xFromCenter / (view.frame.width / 2) * 0.785)
+        people[selectedCardCount].transform = CGAffineTransform(rotationAngle: xFromCenter / (view.frame.width / 2) * 0.785)
+        
+        if xFromCenter > 0 {
+            likeImageView.image = #imageLiteral(resourceName: "good")
+            likeImageView.alpha = 1
+            likeImageView.tintColor = UIColor.red
+        }else if xFromCenter < 0 {
+            likeImageView.image = #imageLiteral(resourceName: "bad")
+            likeImageView.alpha = 1
+            likeImageView.tintColor = UIColor.blue
+        }
+        
+        if sender.state == UIGestureRecognizerState.ended {
+            //左に大きくスワイプ
+            if card.center.x < 75 {
+                UIView.animate(withDuration: 0.2, animations: {
+                    self.resetCard()
+                    self.people[self.selectedCardCount].center = CGPoint(x: self.people[self.selectedCardCount].center.x - 300, y: self.people[self.selectedCardCount].center.y)
+                })
+                likeImageView.alpha = 0
+                selectedCardCount += 1
+                if selectedCardCount >= people.count {
+                    print(likedName)
+                }
+                return
+            //右に大きくスワイプ
+            } else if card.center.x > self.view.frame.width - 75 {
+                UIView.animate(withDuration: 0.2, animations: {
+                    self.resetCard()
+                    self.people[self.selectedCardCount].center = CGPoint(x: self.people[self.selectedCardCount].center.x + 300, y: self.people[self.selectedCardCount].center.y)
+                })
+                likeImageView.alpha = 0
+                likedName.append(name[selectedCardCount])
+                selectedCardCount += 1
+                if selectedCardCount >= people.count {
+                    print(likedName)
+                }
+                return
+            }
+            
+            
+            //元に戻る処理
+            UIView.animate(withDuration: 0.2, animations: {
+                self.resetCard()
+                self.people[self.selectedCardCount].center = self.centerOfCard
+                self.people[self.selectedCardCount].transform = .identity
+            })
+            likeImageView.alpha = 0
+        }
+    }
+    
+
+}
+
